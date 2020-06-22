26ad0c648c02ed2c61f25e3b398cd9cc858338bc
mkopsho <michael.kopsho@gmail.com>
2020-06-22 12:52:54 -0400

    Moving recipe controller to the correct file and added content to its views

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,2 +1,36 @@
 class RecipesController < ApplicationController
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :'recipes/index'
+  end
+
+  get '/recipes/new' do
+    erb :'recipes/new'
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :'recipes/show'
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :'recipes/edit'
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.destroy(params[:id])
+    redirect '/recipes'
+  end
 end
