d6d39bf4940e537cc94982107b66ebabfa7f3a32
mkopsho <michael.kopsho@gmail.com>
2020-06-23 15:54:36 -0400

    Making sure that users can't mess with other recipes

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,45 +1,94 @@
 class RecipesController < ApplicationController
   get '/recipes' do
     if logged_in?
+      @user_recipes = Recipe.all.select { |recipe| recipe.user == current_user }
       @public_recipes = Recipe.all.select { |recipe| recipe.is_public? }
       erb :'recipes/index'
     else
-      erb :homepage
+      redirect '/'
     end
   end
 
   get '/recipes/new' do
-    @ingredients = Ingredient.all
-    erb :'recipes/new'
+    if logged_in?
+      @ingredients = Ingredient.all
+      erb :'recipes/new'
+    else
+      redirect '/'
   end
 
   post '/recipes' do
-    # To do: instantiate an `Ingredient` object for each ingredient listed on the form and shovel into associated recipe method.
-    @recipe = Recipe.create(name: params[:name], description: params[:description])
-    ingredient = Ingredient.create(name: params[:ingredients][:name], quantity: params[:ingredients][:quantity])
-    @recipe.ingredients << ingredient
-    redirect to "/recipes/#{recipe.id}"
+    if logged_in?
+      # To do: instantiate an `Ingredient` object for each ingredient listed on the form (also needs to be built correctly) and shovel into associated recipe method.
+      @recipe = Recipe.create(name: params[:name], description: params[:description], user: current_user)
+      ingredient = Ingredient.create(name: params[:ingredients][:name], quantity: params[:ingredients][:quantity])
+      @recipe.ingredients << ingredient
+      if @recipe.save
+        redirect "/recipes/#{@recipe.id}"
+      else
+        redirect "/recipes/new"
+      end
+      else
+        redirect '/'
+      end
+    end
   end
 
   get '/recipes/:id' do
-    @recipe = Recipe.find(params[:id])
-    erb :'recipes/show'
+    if logged_in?
+      @recipe = Recipe.find_by(id: params[:id])
+      if @recipe.is_public? == true || @recipe.user == current_user
+        erb :'recipes/show'
+      else
+        redirect '/recipes'
+      end
+    else
+      redirect '/'
+    end
   end
 
   get '/recipes/:id/edit' do
-    @recipe = Recipe.find(params[:id])
-    @ingredients = Ingredient.all
-    erb :'recipes/edit'
+    if logged_in?
+      @recipe = Recipe.find_by(id: params[:id])
+      @ingredients = Ingredient.all
+      if @recipe.user == current_user
+        erb :'recipes/edit'
+      else
+        # To do: create/expose flash message about not "owning" the recipe
+        redirect '/recipes'
+      end
+    else
+      redirect '/'
+    end
   end
 
   patch '/recipes/:id' do
-    @recipe = Recipe.find(params[:id])
-    @recipe.update(params)
-    redirect "/recipes/#{@recipe.id}"
+    if logged_in?
+      @recipe = Recipe.find_by(id: params[:id])
+      @recipe.update(params)
+      if @recipe.user == current_user
+        redirect "/recipes/#{@recipe.id}"
+      else
+        # To do: create/expose flash message about not "owning" the recipe
+        redirect '/recipes'
+      end
+    else
+      redirect '/'
+    end
   end
 
   delete '/recipes/:id' do
-    @recipe = Recipe.destroy(params[:id])
-    redirect '/recipes'
+    if logged_in?
+      @recipe = Recipe.find_by(id: params[:id])
+      if @recipe.user == current_user
+        @recipe.destroy
+        redirect '/recipes'
+      else
+        # To do: create/expose flash message about not "owning" the recipe
+        redirect '/recipes'
+      end
+    else
+      redirect '/'
+    end
   end
 end
