d985c1077ab87991248514315a542927671b56e6
mkopsho <michael.kopsho@gmail.com>
2020-06-24 15:54:46 -0400

    I see u

--- app/controllers/ingredient_controller.rb
+++ app/controllers/ingredient_controller.rb
@@ -1,17 +1,25 @@
 class IngredientsController < ApplicationController
   get '/ingredients' do
-    ingredients = Ingredient.all
-    grains = ingredients.select { |ingredient| ingredient.type_of == "grain" }
-    @grain_list = grains.map { |grain| grain.name }
-    hops = ingredients.select { |ingredient| ingredient.type_of == "hops" }
-    @hop_list = hops.map { |hop| hop.name }
-    yeast = ingredients.select { |ingredient| ingredient.type_of == "yeast" }
-    @yeast_list = yeast.map { |yeast| yeast.name }
-    erb :'ingredients/index'
+    if logged_in?
+      ingredients = Ingredient.all
+      grains = ingredients.select { |ingredient| ingredient.type_of == "grain" }
+      @grain_list = grains.map { |grain| grain.name }
+      hops = ingredients.select { |ingredient| ingredient.type_of == "hops" }
+      @hop_list = hops.map { |hop| hop.name }
+      yeast = ingredients.select { |ingredient| ingredient.type_of == "yeast" }
+      @yeast_list = yeast.map { |yeast| yeast.name }
+      erb :'ingredients/index'
+    else
+      redirect '/'
+    end
   end
 
   get '/ingredients/:id' do
-    @ingredient = Ingredient.find(params[:id])
-    erb :'ingredients/show'
+    if logged_in?
+      @ingredient = Ingredient.find(params[:id])
+      erb :'ingredients/show'
+    else
+      redirect '/'
+    end
   end
 end
